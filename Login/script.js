520ee36a02ca31a3a5ea0c20fa77a4fdce4cea63
Caio-okuma <caiookuma10@gmail.com>
2025-08-08 08:52:14 -0300

    Add files via upload

--- Login/script.js
+++ Login/script.js
@@ -1,32 +1,38 @@
-let produtos
+function login(){
+    var nome = $('#nome').val()
+    var senha = $('#senha').val()
 
-window.onload = function() {
-    var storedUser = localStorage.getItem("usuario");
-    var user = JSON.parse(storedUser);
+    if(nome && senha && nome === "admin" && senha === "12345"){
+        const user = {
+            name: nome,
+            dataEntrada: new Date(),
+            id: Math.floor(Math.random() * 100000),
+        };
+        localStorage.setItem("usuario", JSON.stringify(user))
+        window.location.href = "../Loja/index.html"
+    }else{
+        document.getElementById("error-modal").style.display = "flex"
+        document.getElementById("nome").style.borderBottom = "3px solid red"
+        document.getElementById("senha").style.borderBottom = "3px solid red"
+    }
+}
 
-    var dataEntrada = new Date(user.dataEntrada);
-    var dataFormatada = dataEntrada.toLocaleString("pt-BR", {
-        day: "2-digit",
-        month: "2-digit",
-        year: "numeric",
-        hour: "2-digit",
-        minute: "2-digit"
-    })
-
-    document.getElementById("perfil").innerText = dataFormatada;
-    document.getElementById("user").innerText = user.name;
-    document.getElementById("idPerfil").innerText = user.id;
-};
-
-document.addEventListener("DOMContentLoaded", function(){
-    fetch('. ./Dados/mock.json')
-    .then((response) => response.json())
-    .then((data) => {
-        produtos = data
-        console.log(data)
+function fecharError(){
+        document.getElementById("error-modal").style.display = "none"
+        document.getElementById("nome").style.borderBottom = "2px solid #090909"
+        document.getElementById("senha").style.borderBottom = "2px solid #090909"
+}
 
 
+function showPassword(){
+    var input_senha = document.querySelector('#senha')
+    var img_eye = document.querySelector('#eye')
 
-    }).catch((error) => console.error('Erro ao carregar dados', error))
-
-})
+    if(input_senha.getAttribute('type') === 'password'){
+        input_senha.setAttribute('type', 'text')
+        img_eye.setAttribute('src', './Images/eye.png')
+    }else{
+        input_senha.setAttribute('type', 'password')
+        img_eye.setAttribute('src', './Images/eye-not.png')
+    }
+}
---
+++ Loja/script.js
@@ -0,0 +1,32 @@
+let produtos
+
+window.onload = function() {
+    var storedUser = localStorage.getItem("usuario");
+    var user = JSON.parse(storedUser);
+
+    var dataEntrada = new Date(user.dataEntrada);
+    var dataFormatada = dataEntrada.toLocaleString("pt-BR", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit"
+    })
+
+    document.getElementById("perfil").innerText = dataFormatada;
+    document.getElementById("user").innerText = user.name;
+    document.getElementById("idPerfil").innerText = user.id;
+};
+
+document.addEventListener("DOMContentLoaded", function(){
+    fetch('. ./Dados/mock.json')
+    .then((response) => response.json())
+    .then((data) => {
+        produtos = data
+        console.log(data)
+
+
+
+    }).catch((error) => console.error('Erro ao carregar dados', error))
+
+})
