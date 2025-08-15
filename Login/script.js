43ecfd3574e29694ba652beba3b0990570ac1b66
Caio-okuma <caiookuma10@gmail.com>
2025-08-15 08:22:40 -0300

    Add files via upload

--- Login/script.js
+++ Login/script.js
@@ -2,27 +2,27 @@ function login(){
     var nome = $('#nome').val()
     var senha = $('#senha').val()
 
-    if(nome && senha && nome === "admin" && senha === "12345"){
+    if(nome && senha && nome === "admin" && senha === "admin"){
         const user = {
-            name: nome,
+            name:nome,
             dataEntrada: new Date(),
-            id: Math.floor(Math.random() * 100000),
+            id: Math.floor(Math.random() * 100000)
         };
-        localStorage.setItem("usuario", JSON.stringify(user))
-        window.location.href = "../Loja/index.html"
+
+        localStorage.setItem('usuario', JSON.stringify(user))
+        window.location.href = '../Loja/index.html'
     }else{
-        document.getElementById("error-modal").style.display = "flex"
-        document.getElementById("nome").style.borderBottom = "3px solid red"
-        document.getElementById("senha").style.borderBottom = "3px solid red"
+        document.getElementById('error-modal').style.display = 'flex'
+        document.getElementById('nome').style.borderBottom = '3px solid red'
+        document.getElementById('senha').style.borderBottom = '3px solid red'
     }
 }
 
 function fecharError(){
-        document.getElementById("error-modal").style.display = "none"
-        document.getElementById("nome").style.borderBottom = "2px solid #090909"
-        document.getElementById("senha").style.borderBottom = "2px solid #090909"
+        document.getElementById('error-modal').style.display = 'none'
+        document.getElementById('nome').style.borderBottom = '2px solid #090909'
+        document.getElementById('senha').style.borderBottom = '2px solid #090909'
 }
-
 
 function showPassword(){
     var input_senha = document.querySelector('#senha')
@@ -30,9 +30,9 @@ function showPassword(){
 
     if(input_senha.getAttribute('type') === 'password'){
         input_senha.setAttribute('type', 'text')
-        img_eye.setAttribute('src', './Images/eye.png')
+        img_eye.setAttribute('src', './images/eye-not.png')
     }else{
         input_senha.setAttribute('type', 'password')
-        img_eye.setAttribute('src', './Images/eye-not.png')
+        img_eye.setAttribute('src', './images/eye.png')
     }
 }
